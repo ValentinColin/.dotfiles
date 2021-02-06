b9319101fdef4a746feb1a4dd82b18883cc504a9
Valentin <valentin.colin78@gmail.com>
2021-02-06 03:00:09 +0100

    adaptation

--- main.sh
+++ main.sh
@@ -1,7 +1,46 @@
+##############
+### .zshrc ###
+##############
+
+###########
+### GIT ###
+###########
+# -> Load version control information
+autoload -Uz vcs_info
+precmd_vcs_info() { vcs_info }
+precmd_functions+=( precmd_vcs_info )
+
+##############
+### PROMPT ###
+##############
+# -> tuto shell: https://doc.ubuntu-fr.org/tutoriel/script_shell
+
+setopt prompt_subst
+PS1=$'%{\033[01;38;5;240m%}%{\033[01;34m%}%n %{\033[01;38;5;240m%}- %{\033[01;31m%}[%{$vcs_info_msg_0_%}] %{\033[01;32m%}%~ %{\033[01;38;5;240m%}%(!.#.$) %{\033[00m%}'
+# PROMPT=\$vcs_info_msg_0_'%# '
+zstyle ':vcs_info:git:*' formats '%b'
+
+# -> début de ligne du terminal (+ coloration)
+#export PS1="\n${WHITE_BOLD}\u : \w ${YELLOW}**\$(parse_git_branch)**${WHITE_BOLD}\n▸ ${RESET}"
+#PROMPT='$rouge %n %(!.#.%%) '
+#PS1=$'%{\033[01;38;5;240m%}[%{\033[01;34m%}%n %{\033[01;38;5;240m%}> %{\033[01;32m%}%~ %{\033[01;38;5;240m%}] \%(parse_git_branch) %(!.#.$) %{\033[00m%}'
+
 #########################################
 ### Importe tous les fichiers d'alias ###
 #########################################
 
-for file in ~/.dotfiles/*/aliases.sh; do
+for file in ~/git/.dotfiles/*/aliases.sh; do
   source "$file"
 done
+
+### -> Explicit Aliases:
+if false; then
+   source ~/.dotfiles/zsh/explicit_aliases.sh
+fi
+
+### -> fonction de troll
+if false; then
+   source ~/.dotfiles/troll/troll.sh
+fi
+alias troll="source ~/.dotfiles/troll/troll.sh"
+#source /usr/local/opt/powerlevel10k/powerlevel10k.zsh-theme
