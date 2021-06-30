6a5ce4df5e2fe0fc5cb5c41069d73a5a034e9603
Valentin Colin <valentin.colin78@gmail.com>
2021-06-30 22:48:20 +0200

    Prise en charge de Linux

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -1,3 +1,14 @@
+##################
+### Paramètres ###
+##################
+OS=$(uname -s)
+
+if [ $OS = "Linux" ]; then
+	echo "Alias Linux update"
+elif [ $OS = "Darwin" ]; then
+	echo "Alias MacOS update"
+fi
+
 #######################
 ### GENERAL ALIASES ###
 #######################
@@ -9,24 +20,37 @@ alias ....="cd ../../.."
 alias .....="cd ../../../.."
 
 ### Raccourci terminal
-alias reload="exec $SHELL"
-alias quit=exit
-alias finder="open ."
 alias la="ls -a"
 alias ll="ls -al -FG"
+alias reload="exec $SHELL"
+alias quit=exit
 
 ### Raccourci d'ouverture de dossier dans le finder
-alias isep="open ~/Dossier\ ISEP/A1"
-alias thales="cd ~/Dossiers\ PRO/THALES/Algo-Thales"
-alias rc="open ~/git/.dotfiles/"
+if [ "$OS" = "Linux" ]; then
+	alias finder="nautilus ."
+	alias rc="nautilus ~/git/.dotfiles/"
+elif [ "$OS" = "Darwin" ]; then
+	alias finder="open ."
+	alias rc="open ~/git/.dotfiles/"
+	
+	alias isep="open ~/Dossier\ ISEP/A1"
+	alias thales="open ~/Dossiers\ PRO/THALES/Algo-Thales"
+fi
 
 ### Internet
-# $1 doit être un url
-alias chrome="open -a 'Google Chrome' $1"
-alias safari="open -a safari $1"
-alias moodle="open -a safari https://sso-portal.isep.fr"
-alias hpp="open -a safari https://planning-2021.isep.fr/etudiant"
-alias github="open -a safari https://github.com"
+if [ "$OS" = "Linux" ]; then
+	alias moodle="firefox https://sso-portal.isep.fr"
+	alias hpp="firefox https://planning-2021.isep.fr/etudiant"
+	alias github="firefox https://github.com"
+elif [ "$OS" = "Darwin" ]; then
+	# $1 doit être un url
+	alias chrome="open -a 'Google Chrome' $1"
+	alias safari="open -a safari $1"
+	alias moodle="open -a safari https://sso-portal.isep.fr"
+	alias hpp="open -a safari https://planning-2021.isep.fr/etudiant"
+	alias github="open -a safari https://github.com"
+fi
+
 
 #################
 ### Fonctions ###
