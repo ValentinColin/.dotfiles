48b87e0465d856ae6a864c36d50a92eb2935ea12
Valentin Colin <valentin.colin78@gmail.com>
2021-10-02 17:57:53 +0200

    changement de couleur de man

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -2,22 +2,24 @@
 ### CONFIG MAN ###
 ##################
 
-export LESS_TERMCAP_mb=$'\e[1;32m'
-export LESS_TERMCAP_md=$'\e[1;32m'
-export LESS_TERMCAP_me=$'\e[0m'
-export LESS_TERMCAP_se=$'\e[0m'
-export LESS_TERMCAP_so=$'\e[01;33m'
-export LESS_TERMCAP_ue=$'\e[0m'
-export LESS_TERMCAP_us=$'\e[1;4;31m'
+man () {
+    LESS_TERMCAP_md=$'\e[01;31m' \
+    LESS_TERMCAP_me=$'\e[0m' \
+    LESS_TERMCAP_us=$'\e[01;32m' \
+    LESS_TERMCAP_ue=$'\e[0m' \
+    LESS_TERMCAP_so=$'\e[45;93m' \
+    LESS_TERMCAP_se=$'\e[0m' \
+    command man "$@"
+}
 
-# termcap terminfo  
+# termcap terminfo
 # ks      smkx      make the keypad send commands
 # ke      rmkx      make the keypad send digits
 # vb      flash     emit visual bell
 # mb      blink     start blink
 # md      bold      start bold
 # me      sgr0      turn off bold, blink and underline
-# so      smso      start standout (reverse video)
-# se      rmso      stop standout
 # us      smul      start underline
 # ue      rmul      stop underline
+# so      smso      start standout (reverse video)
+# se      rmso      stop standout
