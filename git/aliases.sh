46a32b8d9591765cabb828ae227604498f7bc5a8
Valentin Colin <valentin.colin78@gmail.com>
2021-10-03 17:39:16 +0200

    removal of useless aliases

--- git/aliases.sh
+++ git/aliases.sh
@@ -2,40 +2,23 @@
 ### LISTE DE MES ALIAS PERSO POUR GIT ###
 #########################################
 
-alias gclone="git clone"
-alias ginit="git init"
-
-alias gf="git fetch"
-alias gpl="git pull"
-
-alias d="git diff"
 alias s="git status"
 alias c="git checkout"
-
 alias b="git branch"
-alias cm="git checkout master"
 
-alias ga="git add"
-alias aa="git add --all"
-
-alias com="git commit -m"
-
-alias p="git push"
-#alias pp="git push origin $(git rev-parse --abbrev-ref HEAD)"
-
-alias acp=addCommitPush
+alias acp=addCommitPush-unsigned
 alias acps=addCommitPush-signed
 
-addCommitPush ()
+addCommitPush-unsigned ()
 {
     git add --all;
-    git commit -m "$1";
+    git commit --message "$1";
     git push origin $(git rev-parse --abbrev-ref HEAD)
 }
 
 addCommitPush-signed ()
 {
     git add --all;
-    git commit -S -m "$1";
+    git commit --gpg-sign --message "$1";
     git push origin $(git rev-parse --abbrev-ref HEAD)
 }
